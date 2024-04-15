94584c42cd7a2e4fafd488f742328e2dd37e7159
Alisher Akhmadov <a.alisher@tadi.uz>
2024-04-15 20:13:03 +0500

    ddd

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-import asyncio
+# import asyncio
 from fastapi import FastAPI, Request
 import subprocess
 
+app = FastAPI()
+gate_status = 0
 
-async def open1():
+
+async def open():
     cmd_file_path = 'port1_on.cmd'
     subprocess.call(cmd_file_path, shell=True)
-
-
-async def close1():
     cmd_file_path = 'port1_off.cmd'
     subprocess.call(cmd_file_path, shell=True)
 
 
-async def open2():
+async def close():
     cmd_file_path = 'ikkinchi_on.cmd'
     subprocess.call(cmd_file_path, shell=True)
-
-
-async def close2():
     cmd_file_path = 'ikkinchi_off.cmd'
     subprocess.call(cmd_file_path, shell=True)
 
@@ -28,53 +25,32 @@ async def stop():
     subprocess.call(cmd_file_path, shell=True)
 
 
-app = FastAPI()
-gate_status = 0
-
-
 @app.post("/webhook")
 async def webhook(request: Request):
-    data = await request.json()
     global gate_status
-    if data[0]["camera"] == 19:
 
-        if gate_status == 0:
-            await open1()
-            await asyncio.sleep(15)  ####
-            await close1()
+    # data = await request.json()
+    # if data[0]["camera"] == 19:
+    if gate_status == 0:
+        await open()
+        gate_status += 1
 
-            gate_status += 1
-        elif gate_status == 1:
-            await close1()
-            gate_status -= 1
-
-    if data[0]["camera"] == 20:
-        if gate_status == 0:
-            await open1()
-            await asyncio.sleep(15)  ###
-            await close1()
-
-            gate_status += 1
-        elif gate_status == 1:
-            await close1()
-            gate_status -= 1
+    elif gate_status == 1:
+        await close()
+        gate_status -= 1
 
     return {"message": "Okey"}
 
 
 @app.post("/open")
 async def open_button():
-    await open1()
-    await asyncio.sleep(1)
-    await close1()
+    await open()
     return 'its opened'
 
 
 @app.post("/close")
 async def close_button():
-    await open2()
-    await asyncio.sleep(1)
-    await close2()
+    await close()
     return 'its closed'
 
 
